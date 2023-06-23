d56ca16f01f87bff00090a5bd8f06508b6cc1cfc
Luigi <56728183+BRLplays@users.noreply.github.com>
2023-06-23 00:41:25 -0400

    Add files via upload

--- pyTorch CIFAR100 - OOD.py
+++ pyTorch CIFAR100 - OOD.py
@@ -16,35 +16,39 @@ from ReliabilityDiagram import _calculate_ece, make_model_diagrams
 
 
 
+
 # Device Configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparameters
-batch_size = 4
+batch_size = 10
 learning_rate = 0.01
 
 # Transform
 data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize(size=(32, 32)),
+        transforms.Resize((32, 32)),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ]),
     'test': transforms.Compose([
-        transforms.Resize(size=(32, 32)),
+        transforms.Resize((32, 32)),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+        transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))
     ]),
 }
 
+
+
 # Dataset
-train_dataset = tv.datasets.CIFAR10(root="C:\Python39\Datasets", train=True,
+train_dataset = tv.datasets.CIFAR100(root="C:\Python39\Datasets", train=True,
                                     download=True, transform=data_transforms['train'])
 
-train_data, val_data = torch.utils.data.random_split(train_dataset, [46000, 4000])
 
+train_data, val_data = torch.utils.data.random_split(train_dataset, [48000, 2000])
 
-ID_test_dataset = tv.datasets.CIFAR10(root="C:\Python39\Datasets", train=False,
+ID_test_dataset = tv.datasets.CIFAR100(root="C:\Python39\Datasets", train=False,
                                     download=True, transform=data_transforms['test'])
 
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
@@ -52,7 +56,6 @@ train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sh
 val_loader = torch.utils.data.DataLoader(val_data, batch_size=batch_size, shuffle=True)
 
 ID_test_loader = torch.utils.data.DataLoader(ID_test_dataset, batch_size=batch_size, shuffle=False)
-
 
 OOD_test_dataset = Textures(root="data", download=True, transform=data_transforms['test'], target_transform=ToUnknown())
 
@@ -70,10 +73,6 @@ dataset_sizes = {
     'train': batch_size*len(train_loader),
     'val': batch_size*len(val_loader)
 }
-
-classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog",
-           "horse", "ship", "truck")
-
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
@@ -155,9 +154,9 @@ class CNN(nn.Module):
         self.conv2 = nn.Conv2d(6, 16, 5) # input size must be equal to previous output size (conv1)
         self.bat2 = nn.BatchNorm2d(16)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(16*5*5, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 10)
+        self.fc1 = nn.Linear(16*5*5, 500)
+        self.fc2 = nn.Linear(500, 300)
+        self.fc3 = nn.Linear(300, 100)
         self.dropout = nn.Dropout(0.2)
     
     def forward(self, x):
@@ -173,11 +172,11 @@ class CNN(nn.Module):
     
 
 # Own model to test
+model = CNN().to(device)
 
-#model = CNN().to(device)
 # Source: https://github.com/chenyaofo/pytorch-cifar-models
-model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_vgg16_bn", pretrained=True).to(device)
-#model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar10_resnet56", pretrained=True).to(device)
+#model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar100_shufflenetv2_x2_0", pretrained=True).to(device)
+#model = torch.hub.load("chenyaofo/pytorch-cifar-models", "cifar100_resnet56", pretrained=True).to(device)
 
 # Loss and optimization
 criterion = nn.CrossEntropyLoss()
@@ -187,9 +186,10 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 # Decay LR by a factor of 0.1 every 10 epochs
 # Learning rate scheduling should be applied after optimizer’s update
 
-step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
+step_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1)
 
-model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=12)
+model = train_model(model, criterion, optimizer, step_lr_scheduler, num_epochs=150)
+
 model.eval()
 
 # Testing
@@ -218,20 +218,9 @@ with torch.no_grad():
             
             outs = torch.cat((outs, outputs[i]), 0)
             trues_list.append(label.item())
-            
-            if(label == pred):
-                n_class_correct[label] += 1
-            
-            n_class_samples[label] += 1
-            
-            
-    acc = 100.0 * n_correct / n_samples
-    print(f"ID Accuracy of CNN: {acc}%")
-        
-    for i in range(10):
-        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-        print(f"Accuracy of {classes[i]}: {acc}%")
 
+    acc = 100.0 * n_correct / n_samples
+    print(f"\nOOD Accuracy of CNN: {acc}%")
 
 
 # Using OOD data
@@ -242,6 +231,7 @@ with torch.no_grad():
     n_class_samples = [0 for i in range(10)]
     
     for images, labels in OOD_test_loader:
+        #print(images.shape())
         images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
@@ -276,7 +266,7 @@ with torch.no_grad():
         n_correct += (predicted == labels).sum().item()          
             
     acc = 100.0 * n_correct / n_samples
-    print(f"\Mixed Accuracy of CNN: {acc}%")
+    print(f"\nMixed Accuracy of CNN: {acc}%\n")
 
 
 
@@ -294,7 +284,7 @@ print(metrics.compute())
 
 
 # Reliability Diagram
-outs = outs.view(-1, 10)
+outs = outs.view(-1, 100)
 trues = torch.Tensor(trues_list)
 make_model_diagrams(outs, trues, 10)
 
